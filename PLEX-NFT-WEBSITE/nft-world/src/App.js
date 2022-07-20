f5c58aae886f8d4f80e3435492c66190a4d90553
imoja021 <imoja021@student.wethinkcode.co.za>
2022-07-20 19:13:37 +0200

    set up is complete

--- PLEX-NFT-WEBSITE/nft-world/src/App.js
+++ PLEX-NFT-WEBSITE/nft-world/src/App.js
@@ -1,5 +1,28 @@
 import React from "react";
+import Clients from "./components/Clients";
+import Footer from "./components/Footer";
+import Free from "./components/Free";
+import Home from "./components/Home";
+import Like from "./components/Like";
+import Navbar from "./components/Navbar";
+import Release from "./components/Release";
+import ScrollTop from "./components/ScrollTop";
+import SignUp from "./components/SignUp";
+import SuperRare from "./components/SuperRare";
+import "./scss/index.scss";
 
 export default function App(){
-  return <div>hello</div>
+  return (
+  <div className = "app-container">
+    <ScrollTop/>
+    <Navbar/>
+    <Home/>
+    <Free/>
+    <Clients/>
+    <SuperRare/>
+    <Release/>
+    <Like/>
+    <SignUp/>
+    <Footer/>    
+  </div>);
 }
