e2dccf25721fbabd5f297b40cc73025a59ec8164
imoja021 <imoja021@student.wethinkcode.co.za>
2022-07-21 22:13:26 +0200

    home and navabar almost complete

--- PLEX-NFT-WEBSITE/nft-world/src/components/Home.jsx
+++ PLEX-NFT-WEBSITE/nft-world/src/components/Home.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import home from "../assets/home.png"
 
 export default function Home(){
-  return <div></div>
+  return (
+    <div className="home">
+      <div className="container">
+        <div className="content">
+          <p className="sub_title">Coming soon</p>
+          <h1 className="title">Introducing Plex nft</h1>
+          <p className="description">
+            Explore the amazing world of PTM. Sign up below to
+            recieve updates.
+          </p>
+          <button>Sign Up here</button>
+        </div>
+        <div className="image_container">
+          <div className="image">
+            <img src={home} alt="home" />
+          </div>
+          <div className="ellipse_container">
+            <div className="ellipse blue"></div>
+            <div className="ellipse purple"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
