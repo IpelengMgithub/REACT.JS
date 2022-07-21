84827d11aaadfbc4b22afc2a32a9a3a765fe303a
imoja021 <imoja021@student.wethinkcode.co.za>
2022-07-21 20:52:53 +0200

    navbar complete

--- PLEX-NFT-WEBSITE/nft-world/src/components/Navbar.jsx
+++ PLEX-NFT-WEBSITE/nft-world/src/components/Navbar.jsx
@@ -1,5 +1,58 @@
-import React from "react";
+import React ,{useState} from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdClose } from "react-icons/md";
+import { ImSun } from "react-icons/im";
+import { BsFillMoonFill } from "react-icons/bs";
+import logo from "../assets/logo.png";
 
-export default function Navbar(){
-  return <div></div>
+export default function Navbar({ changeTheme, currentTheme }){
+  const [navState, setNavState] = useState(false);
+  return (
+    <nav>
+      <div className="brand_contain">
+        <div className="brand_">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="toggle_contain">
+          <div className="toggle_">
+            {navState ? (
+              <MdClose onClick={() => setNavState(false)} />
+            ) : (
+              <GiHamburgerMenu onClick={() => setNavState(true)} />
+            )}
+          </div>
+          <div className="mode_" onClick={changeTheme}>
+            {currentTheme === "dark" ? (
+              <ImSun className="light" />
+            ) : (
+              <BsFillMoonFill className="dark" />
+            )}
+          </div>
+        </div>
+      </div>
+      <div className={`links_contain ${navState ? "nav_visible" : ""}`}>
+        <ul className="links_">
+          <li>
+            <a href="#features">Features-nft</a>
+          </li>
+          <li>
+            <a href="#about">About-section</a>
+          </li>
+          <li>
+            <a href="#launch">Launch</a>
+          </li>
+          <li>
+            <a href="#signup">Sign Up</a>
+          </li>
+          <li onClick={changeTheme}>
+            {currentTheme === "dark" ? (
+              <ImSun className="light" />
+            ) : (
+              <BsFillMoonFill className="dark" />
+            )}
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
